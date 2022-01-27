481d6bde3a6faff2031eb323dc90c5619293acb0
Jonathan Hrang <jsianguk@gmail.com>
2022-01-26 22:33:01 -0500

    first
    first

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'prepare.dart';
+import 'package:blogsquid/utils/globals.dart' as globals;
+
+void main() {
+  globals.appNavigator = GlobalKey<NavigatorState>();
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+  // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+  //     statusBarColor: Colors.transparent, // transparent status bar
+  //     statusBarIconBrightness: Brightness.dark // dark text for status bar
+  //     ));
+  runApp(ProviderScope(child: MyApp()));
+}
+
+class MyApp extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    useEffect(() {
+      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarColor: Colors.black,
+        systemNavigationBarColor: Colors.black,
+      ));
+    }, const []);
+    return MaterialApp(
+      title: 'Blog Squid',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: "Circular",
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      darkTheme: ThemeData(
+        scaffoldBackgroundColor: Colors.black,
+        fontFamily: "Circular",
+      ),
+
+      /// navigatorKey: globals.appNavigator,
+      home: Prepare(),
+    );
+  }
+}
---
+++ lib/pages/dashboard.dart
@@ -0,0 +1,119 @@
+import 'package:blogsquid/config/app.dart';
+import 'package:blogsquid/pages/categories/subCategories.dart';
+import 'package:blogsquid/pages/tabs/account.dart';
+import 'package:blogsquid/pages/tabs/bookmarks.dart';
+import 'package:blogsquid/pages/tabs/categories.dart';
+import 'package:blogsquid/pages/tabs/search.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+
+import 'tabs/home.dart';
+import 'tabs/partials/animated_bottom_bar.dart';
+
+class Dashboard extends StatefulWidget {
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard>
+    with SingleTickerProviderStateMixin {
+  late PersistentTabController controller;
+  late double timeDilation;
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: SvgPicture.asset(
+          iconsPath + "home.svg",
+          color: Colors.deepOrange,
+        ),
+        inactiveIcon: SvgPicture.asset(
+          iconsPath + "home.svg",
+          color: Colors.grey,
+        ),
+      ),
+      PersistentBottomNavBarItem(
+        icon: SvgPicture.asset(iconsPath + "collection.svg",
+            color: Colors.deepOrange),
+        inactiveIcon: SvgPicture.asset(
+          iconsPath + "collection.svg",
+          color: Colors.grey,
+        ),
+      ),
+      PersistentBottomNavBarItem(
+        icon: SvgPicture.asset(iconsPath + "search.svg",
+            color: Colors.deepOrange),
+        inactiveIcon: SvgPicture.asset(
+          iconsPath + "search.svg",
+          color: Colors.grey,
+        ),
+      ),
+      PersistentBottomNavBarItem(
+        icon: SvgPicture.asset(iconsPath + "bookmark.svg",
+            color: Colors.deepOrange),
+        inactiveIcon: SvgPicture.asset(
+          iconsPath + "bookmark.svg",
+          color: Colors.grey,
+        ),
+      ),
+      PersistentBottomNavBarItem(
+        icon: SvgPicture.asset(
+          iconsPath + "user-circle.svg",
+          color: Colors.deepOrange,
+        ),
+        inactiveIcon: SvgPicture.asset(
+          iconsPath + "user-circle.svg",
+          color: Colors.grey,
+        ),
+      ),
+    ];
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller = PersistentTabController(initialIndex: 0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      body: PersistentTabView(
+        context,
+        controller: controller,
+        confineInSafeArea: true,
+        backgroundColor: Colors.white,
+        handleAndroidBackButtonPress: true,
+        resizeToAvoidBottomInset: true,
+        stateManagement: true,
+        navBarStyle: NavBarStyle.simple,
+        items: _navBarsItems(),
+        screens: [
+          Container(
+              color: Colors.white,
+              height: MediaQuery.of(context).size.height,
+              child: Home()),
+          Container(
+              color: Colors.white,
+              height: MediaQuery.of(context).size.height,
+              child: Categories()),
+          Container(
+              color: Colors.white,
+              height: MediaQuery.of(context).size.height,
+              child: Search()),
+          Container(
+              color: Colors.white,
+              height: MediaQuery.of(context).size.height,
+              child: Bookmarks()),
+          Container(
+              color: Colors.white,
+              height: MediaQuery.of(context).size.height,
+              child: Account()),
+        ],
+      ),
+    );
+  }
+}
